83adb79de0b02ece03f3169649c1d35d2a785c8f
OptikCrash <Christopher.Nacey@dfw.wa.gov>
2022-04-25 08:06:16 -0700

    hello

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,31 +8,61 @@ fun main(args: Array<String>) {
 fun sumOfMultiples(first: Int, second: Int, limit: Int): Int {
     var i = 0
     var sum = 0
-    val list = mutableListOf<Int>()
     while (i < limit) {
         if ( i % first == 0 || i % second == 0) {
-            list.add(i)
             sum+= i
         }
         i++
     }
-    println("list = $list")
     println("sum = $sum")
     return sum
 }
 
 fun sumOfEvenFibonacci(limit: Int): Int {
-    var t1 = 0
-    var t2 = 1
-    var result = 0
-
-    while (t1 <= limit) {
-        val sum = t1 + t2
-        t1 = t2
-        t2 = sum
-        if (sum % 2 == 0) {
-            result+= sum
+    var sum : Int = 0
+    fibonacciToN(limit).forEach {
+        if (it % 2 == 0) {
+            sum += it
         }
     }
-    return result
+    return sum
 }
+
+fun fibonacciToN(n: Int) : MutableList<Int> {
+    var first = 0
+    var second = 1
+    var list = mutableListOf(first, second)
+    while (first <= n) {
+        val sum = first + second
+        list.add(sum)
+        first = second
+        second = sum
+    }
+    return list
+}
+
+fun largestPrimeFactor(finalProduct: Int) {
+
+}
+
+fun primeToN(n: Int) : MutableList<Int> {
+    val primes = mutableListOf<Int>()
+    for (i in 1..n) {
+        if (i.isPrime()) {
+            primes.add(i)
+        }
+    }
+    return primes
+}
+
+fun Int.isPrime() : Boolean {
+    var flag = false
+    for (i in 2..this / 2) {
+        if (this % i == 0) {
+            flag = true
+            break
+        }
+    }
+    return !flag
+}
+
